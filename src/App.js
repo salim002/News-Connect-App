f346521cbeaf2d2794206fbed9eb869ffc177a55
MD SALIM <mdsalim.iiitr@gmail.com>
2023-06-14 15:21:30 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+
+import React, {useState} from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+import LoadingBar from 'react-top-loading-bar'
+
+const App = ()=>{
+  const pageSize = 5;
+  // const apiKey=process.env.REACT_APP_NEWS_API
+  const apiKey="4f560709ba824e9b9e0199fc6558c506"
+  
+  const [progress, setProgress] = useState(0)
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <LoadingBar
+            height={3}
+            color='#f11946'
+            progress={progress}
+          />
+          <Routes>
+            <Route exact path='/' element={<News setProgress={setProgress} apiKey={apiKey} key='general' pageSize={pageSize} country='in' category='general' />}></Route>
+            <Route exact path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key='business' pageSize={pageSize} country='in' category='business' />}></Route>
+            <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key='entertainment' pageSize={pageSize} country='in' category='entertainment' />}></Route>
+            <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key='health' pageSize={pageSize} country='in' category='health' />}></Route>
+            <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key='science' pageSize={pageSize} country='in' category='science' />}></Route>
+            <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key='sports' pageSize={pageSize} country='in' category='sports' />}></Route>
+            <Route exact path='/technology' element={<News setProgress={setProgress} apiKey={apiKey} key='technology' pageSize={pageSize} country='in' category='technology' />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    )
+}
+
+export default App;
---
+++ src/components/NewsItem.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const NewsItem = (props)=> {
+    let {title, description, imageUrl, newsUrl, author, date, source} = props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            position: 'absolute',
+            right: '0'
+          }}>
+            <span className="badge rounded-pill bg-danger">{source}</span>
+            
+          </div>
+          <img src={!imageUrl?"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png":imageUrl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text"><small className="text-body-secondary">By {author?author:"unknown"} on {new Date(date).toGMTString()} </small></p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default NewsItem
